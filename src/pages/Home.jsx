fea0059e0c10e175d5d01b49b38391950aa6a9e5
JerryDcodemaster <ekeminijoseph83@gmail.com>
2024-04-23 15:09:27 +0100

    Resize divs width on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import ImageSlider from '../components/ImageSlider';
 import Carousel from '../components/Carousel';
 import ExpertsCard from '../components/ExpertsCard';
+import Image1 from '../bg-home1.png'
+import Image2 from '../bg-home2.png'
+import Image3 from '../bg-home3.png'
+import Image4 from '../bg-home4.png'
+
 
 const Home = () => {
   const [text] = useTypewriter({
@@ -12,40 +17,38 @@ const Home = () => {
     deleteSpeed: 80,
   });
 
+  const images = [Image1, Image2, Image3, Image4];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // useEffect(() => {
+  //     const intervalId = setInterval(() => {
+  //         if(currentIndex === images.length - 1) {
+  //             setCurrentIndex(0);
+  //         } 
+  //         else {
+  //               setCurrentIndex(currentIndex + 1);
+  //         }
+  //     }, 5000)
+      
+  //     return () => clearInterval(intervalId);
+  //   }, []
+  // )
+
+  useEffect(() => {
+    setInterval(() => {
+      let random = Math.floor(Math.random() * 4);
+      setCurrentIndex(random);
+    }, 3000)
+  }, [])
+
+
   return (
     <div>
-      {/* <section className="section-1 row align-items-center mb-3">
-        <div className='col-xl-6 col-md-12'>
-          <h2 className="poppins-bold multi-head">Find the right <span className="multicolor-text">Expert</span></h2>
-          <span className="poppins-regular type-text">to interview {' '}
-            <span>{text}</span>
-            <span className="cursor-type">
-              <Cursor cursorStyle='|' />
-            </span>
-          </span>
-
-          <div class="input-group search-bar mt-4 pb-4">
-            <input type="text" class="form-control search-input" placeholder="Search for any service..." aria-label="Recipient's username" aria-describedby="button-addon2" />
-            <button class="btn btn-outline-success" type="button" id="button-addon2">
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </div>
-
-          <div className='d-flex gap-2 product-tag'>
-            <span className="fw-bold">Popular:</span>
-            <button>Product Design</button>
-            <button>Software Dev</button>
-            <button>Data Analysis</button>
-          </div>
-
-        </div>
-
-        <img id='image' className='col-xl-6 col-md-6' src="./images/bg-home1.png" alt="" />
-      </section> */}
-
       <section class="hero-section mb-3">
         <div class="content">
-            <h1>Find the right <span class="multicolor-text">Expert</span></h1>
+            <h1 className='show-head-text-large'>Find the right <span class="multicolor-text">Expert</span></h1>
+            <h2 className='show-head-text-mobile'>Find the right <span class="multicolor-text-mobile">expert</span> to interview your candidate</h2>
 
             <p className="poppins-regular type-text">to interview {' '}
               <span>{text}</span>
@@ -68,8 +71,9 @@ const Home = () => {
                 </ul>
             </div>
         </div>
-        <img src="./images/bg-home1.png" alt="" />
-    </section>
+        {/* <img src="./images/bg-home1.png" alt="" /> */}
+        <img src={images[currentIndex]} />
+      </section>
 
       <section className="brand-companies container d-flex justify-content-center align-items-center gap-0 gap-md-3 gap-xl-5">
         <span className='brand-relative'>Trusted by:</span>
@@ -85,24 +89,22 @@ const Home = () => {
         <ImageSlider />
       </section>
 
-    <div className='section-3'>
-      <section className="row mb-4 gap-3">
-        <div className="col-xl-6">
-          <h2 className="fw-bold crimson-pro mb-5">What we have to offer?</h2>
-          <img className='mb-2' src="./images/1txt.png" alt="" />
-          <img className='mb-2' src="./images/2txt.png" alt="" />
-          <img className='mb-2' src="./images/3txt.png" alt="" />
-          <img src="./images/4txt.png" alt="" />
-        </div>
+      <div className='section-3'>
+        <section className="row mb-4 gap-3">
+          <div className="col-xl-6">
+            <h2 className="fw-bold crimson-pro mb-5">What we have to offer?</h2>
+            <img className='mb-2' src="./images/1txt.png" alt="" />
+            <img className='mb-2' src="./images/2txt.png" alt="" />
+            <img className='mb-2' src="./images/3txt.png" alt="" />
+            <img src="./images/4txt.png" alt="" />
+          </div>
 
-        <img className='col h-md-25' src="./images/bg-sec3.png" alt="" />
-      </section>
+          <img className='col h-md-25' src="./images/bg-sec3.png" alt="" />
+        </section>
 
-      <div class="d-grid gap-2 col-xl-6 col-md-12 mx-auto">
-        <button class="get-started" type="button">
-          <a className='a-2' href="#!">Get Started</a>
+        <button class="get-started-offerbtn" type="button">
+          <a className='a-2' id='offer-btn' href="#!">Get Started</a>
         </button>
-      </div>
       </div>
 
       <section className="section-4">
@@ -155,8 +157,8 @@ const Home = () => {
           <img className='col' src="./images/sec5-img.png" alt="" />
         </section>
 
-        <div class="d-grid gap-2 col-xl-6 col-md-12 mx-auto">
-          <button class="get-started" type="button">
+        <div class="getstarted">
+          <button class="get-started-revobtn" type="button">
             <a className='a-2' href="#!">Get Started</a>
           </button>
         </div>
@@ -168,11 +170,9 @@ const Home = () => {
 
       <section className="section-7 row mb-5 d-flex justify-content-center align-items-center">
         <div className="col-xl col-sm-6 col-md">
-          {/* <img src="./images/as-a-recruiter.png" alt="" className='w-25 mb-2' /> */}
           <span className="recruit-text">As a Recruiter</span>
           <p className="relax-text">Relax, while we handle recruiting for you</p>
-          {/* <img src="./images/relax.png" alt="" className="w-100 mb-3" /> */}
-          {/* <button className='join-now'><a className='a-2' href="#!">Join now</a></button> */}
+          <button className='join-now'><a className='a-2' href="#!">Join now</a></button>
         </div>
         
         <img src="./images/unsplash.png" alt="" className="col-xl col-md" />
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -3,148 +3,150 @@ import '../css/pricing.css';
 
 const Pricing = () => {
   return (
-    <div className="container py-5">
-      <h4 className='poppins-semibold head-text'>Choose Your Subscription plan</h4>
+    <div className='pricing-div'>
+      <div className="container py-5">
+        <h4 className='poppins-semibold head-text'>Choose Your Subscription plan</h4>
 
-      <section className="card-section">
-        <h4 className="crimson-pro-font small-text">For Jobseekers</h4>
+        <section className="card-section">
+          <h4 className="crimson-pro-font small-text">For Jobseekers</h4>
 
-        <div className="pricing-container">
-          <div className="card">
-            <h4 className='poppins-semibold mb-5'>Basic</h4>
-            <h4 className='poppins-semibold free'>Free</h4>
+          <div className="pricing-container">
+            <div className="card">
+              <h4 className='poppins-semibold mb-5'>Basic</h4>
+              <h4 className='poppins-semibold free'>Free</h4>
 
-            <span className='d-flex align-items-center pt-2 pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Join for free</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay per interview session with experts</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for career advice with experts</span>
-            </span>
+              <span className='d-flex align-items-center pt-2 pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Join for free</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay per interview session with experts</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for career advice with experts</span>
+              </span>
 
-            <button className='pricing-btn mt-2'><a className='a-session' href="#!">Get Started</a></button>
+              <button className='pricing-btn mt-2'><a className='a-session' href="#!">Get Started</a></button>
+            </div>
+
+            <div className="card second-card">
+              <h4 className='poppins-semibold'>Standard</h4>
+              <button className="tag">Most Popular</button>
+              <h4 className='poppins-semibold free'>$29 <span className='text-dark fs-6'>USD /month</span></h4>
+
+              <span className='d-flex align-items-center pt-2 pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2"> Join for free</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay per interview session with experts</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for career advice with experts</span>
+              </span>
+
+              <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
+            </div>
+
+            <div className="card">
+              <h4 className='poppins-semibold'>Pro</h4>
+              <button className="tag">Save $30</button>
+              <h4 className='poppins-semibold free'>$150 <span className='text-dark fs-6'>USD /6 months</span></h4>
+
+              <span className='d-flex align-items-center pt-2 pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2"> Join for free</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay per interview session with experts</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for career advice with experts</span>
+              </span>
+
+              <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
+            </div>
+
+            
           </div>
+        </section>
 
-          <div className="card second-card">
-            <h4 className='poppins-semibold'>Standard</h4>
-            <button className="tag">Most Popular</button>
-            <h4 className='poppins-semibold free'>$29 <span className='text-dark fs-6'>USD /month</span></h4>
+        <section className="card-section">
+          <h4 className="crimson-pro-font small-text">For Recruiters</h4>
 
-            <span className='d-flex align-items-center pt-2 pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2"> Join for free</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay per interview session with experts</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for career advice with experts</span>
-            </span>
+          <div className="pricing-container">
+            <div className="card">
+              <h4 className='poppins-semibold mb-5'>Pay per session</h4>
+              <h4 className='poppins-semibold free mb-2'>Flexible</h4>
 
-            <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
+              <span className='d-flex align-items-center pt-2 pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for individual candidate</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay per interview session to experts</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for career advice with experts</span>
+              </span>
+
+              <button className='pricing-btn mt-5'><a className='a-session' href="#!">Get Started</a></button>
+            </div>
+
+            <div className="card second-card">
+              <h4 className='poppins-semibold'>Standard</h4>
+              <button className="tag">Most Popular</button>
+              <h4 className='poppins-semibold free'>$29 <span className='text-dark fs-6'>USD /10 Candidates</span></h4>
+
+              <span className='d-flex align-items-center pt-2 pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for interview sessions of 10 candidates</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Get feedback from interviewer of the sponsored candidates</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Get notified when session begins</span>
+              </span>
+
+              <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
+            </div>
+
+            <div className="card">
+              <h4 className='poppins-semibold'>Pro</h4>
+              <button className="tag">Save $30</button>
+              <h4 className='poppins-semibold free'>$49 <span className='text-dark fs-6'>USD /20 Candidates</span></h4>
+
+              <span className='d-flex align-items-center pt-2 pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Pay for interview sessions of 20 candidates</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">et feedback from interviewer of the sponsored candidates</span>
+              </span>
+              <span className='d-flex align-items-center pb-3'>
+                <i class="fa-solid fa-check"></i>
+                <span className="poppins-reg px-2">Get notified when session begins</span>
+              </span>
+
+              <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
+            </div>
+
+            
           </div>
-
-          <div className="card">
-            <h4 className='poppins-semibold'>Pro</h4>
-            <button className="tag">Save $30</button>
-            <h4 className='poppins-semibold free'>$150 <span className='text-dark fs-6'>USD /6 months</span></h4>
-
-            <span className='d-flex align-items-center pt-2 pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2"> Join for free</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay per interview session with experts</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for career advice with experts</span>
-            </span>
-
-            <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
-          </div>
-
-          
-        </div>
-      </section>
-
-      <section className="card-section">
-        <h4 className="crimson-pro-font small-text">For Recruiters</h4>
-
-        <div className="pricing-container">
-          <div className="card">
-            <h4 className='poppins-semibold mb-5'>Pay per session</h4>
-            <h4 className='poppins-semibold free mb-2'>Flexible</h4>
-
-            <span className='d-flex align-items-center pt-2 pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for individual candidate</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay per interview session to experts</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for career advice with experts</span>
-            </span>
-
-            <button className='pricing-btn mt-5'><a className='a-session' href="#!">Get Started</a></button>
-          </div>
-
-          <div className="card second-card">
-            <h4 className='poppins-semibold'>Standard</h4>
-            <button className="tag">Most Popular</button>
-            <h4 className='poppins-semibold free'>$29 <span className='text-dark fs-6'>USD /10 Candidates</span></h4>
-
-            <span className='d-flex align-items-center pt-2 pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for interview sessions of 10 candidates</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Get feedback from interviewer of the sponsored candidates</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Get notified when session begins</span>
-            </span>
-
-            <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
-          </div>
-
-          <div className="card">
-            <h4 className='poppins-semibold'>Pro</h4>
-            <button className="tag">Save $30</button>
-            <h4 className='poppins-semibold free'>$49 <span className='text-dark fs-6'>USD /20 Candidates</span></h4>
-
-            <span className='d-flex align-items-center pt-2 pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Pay for interview sessions of 20 candidates</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">et feedback from interviewer of the sponsored candidates</span>
-            </span>
-            <span className='d-flex align-items-center pb-3'>
-              <i class="fa-solid fa-check"></i>
-              <span className="poppins-reg px-2">Get notified when session begins</span>
-            </span>
-
-            <button className='pricing-btn'><a className='a-session' href="#!">Get Started</a></button>
-          </div>
-
-          
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
 
 
